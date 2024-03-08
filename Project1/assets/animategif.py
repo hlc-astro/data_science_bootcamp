f76f3130d95cbe6e7fc34a88641159ea0ab6f8a0
Holly Capelo <holly.capelo@caa.columbiaedu>
2024-03-08 21:57:44 +0100

    changed duration of gif playback for project1 readme

--- Project1/assets/animategif.py
+++ Project1/assets/animategif.py
@@ -3,6 +3,7 @@ import os
 input_folder="/Users/capelo/Desktop/constructor/portfolio/data_science_bootcamp/Project1/assets/data_challenge_intro"
 output_file="challenge_animaition"
 
+'''
 def merge_gifs(input_folder, output_file):
     gif_files = [file for file in os.listdir(input_folder) if file.endswith('.gif')]
     frames = []
@@ -17,3 +18,22 @@ def merge_gifs(input_folder, output_file):
 #output_file = 'merged.gif'
 
 merge_gifs(input_folder, output_file)
+'''
+
+
+import imageio
+import os
+
+def merge_gifs(input_folder, output_file, frame_duration):
+    gif_files = [file for file in os.listdir(input_folder) if file.endswith('.gif')]
+    frames = []
+    for file_name in gif_files:
+        file_path = os.path.join(input_folder, file_name)
+        frames.append(imageio.imread(file_path))
+    imageio.mimsave(output_file, frames, 'GIF', duration=frame_duration)
+
+
+# Replace 'frame_duration' with the desired duration for each frame (in seconds)
+frame_duration = 2.5
+
+merge_gifs(input_folder, output_file, frame_duration)
