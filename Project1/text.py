2607cd3c087e5299711ef21216a8aa38dce55ad3
Holly Capelo <holly.capelo@caa.columbiaedu>
2024-03-08 14:06:48 +0100

    added docstrings to pythonprograms

--- Project1/text.py
+++ Project1/text.py
@@ -1,10 +1,22 @@
+
+'''
+author: Holly L. Capelo
+
+Program using the open source python package 'tweepy' to obtain twitter data using the API. This program can be used to check if the connection to the API is working and obtain a few tweets from a specified user. The screen_name can be changed as wished. 
+
+'''
+
+
+
 import tweepy
 
 # Authenticate to Twitter
-consumer_key = "ApPHYjEZqQRUttOZ31O1n0fRx"
-consumer_secret = "VMcLhJAvjJgFYWYfEKiQWelpKxSGDRM2EzMIDZwV2FdOG7tAf7"
-access_token = "102588195-xY0HTCFZ1ZTnoY5pP1YhhLHzHAMpVkFUMU5YWI9f"
-access_token_secret = "rvUbjlthxbMWm47zCpfYs086qAPJQLSHP1RgAMApJ2jV8"
+#Note that the AUTHENTICATION data is not shared and the credentials for an active account should be put in place. 
+
+consumer_key = " "
+consumer_secret = " "
+access_token = " "
+access_token_secret = " "
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
